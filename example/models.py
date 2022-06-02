0c6dc41ace9002276a2b12db62a08b278046a10f
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-01 23:09:44 -0400

    made migrations of the models.

--- example/models.py
+++ example/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
-class NewsItem(models.Mode):
+class NewsItem(models.Model):
     title = models.CharField(max_length=30)
     body = models.TextField()
     date = models.DateTimeField(default=timezone.now)
