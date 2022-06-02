ecdd5f420d244a69682493ce84ea8a31023791fa
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-01 23:21:03 -0400

    modified models.py to add function to display objects.

--- example/models.py
+++ example/models.py
@@ -4,8 +4,14 @@ from django.utils import timezone
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class NewsItem(models.Model):
     title = models.CharField(max_length=30)
     body = models.TextField()
     date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
