96c5a1d7ec78447ea0156df6eff2dad5b1c40f97
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-01 22:42:53 -0400

    modified example directories by adding urls.py and modifying settings.py.

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from example.urls import urlpatterns as ex_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# urlpatterns += ex_urls
