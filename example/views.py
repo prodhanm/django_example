3801024bdc02e95737f699432b5f31b5e3e1c568
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-02 20:18:52 -0400

    created the newsaddform.html.

--- example/views.py
+++ example/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from example.models import NewsItem
+from example.forms import NewsAddForm
 
 def index(request):
     data = NewsItem.objects.all()
     html = "index.html"
     context = {'data': data}
+    return render(request, html, context)
+
+def news_add(request):
+    html = "newsaddform.html"
+    form = NewsAddForm()
+    context = {"form": form}
+
     return render(request, html, context)
