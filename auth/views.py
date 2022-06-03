f49a0972d852ff538e783fc89775b6f78013da92
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-03 19:28:06 -0400

    setup the loginview for login authentication.

--- auth/views.py
+++ auth/views.py
@@ -4,7 +4,26 @@ from django.contrib.auth import login, logout, authenticate
 from auth.forms import LoginForm
 
 def login_view(request):
-    pass
+    html = "loginview.html"
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            ''' The user object is the code used to match username and 
+            password.'''
+            user = authenticate(
+                request, 
+                username=data["username"],
+                password=data["password"]
+            )
+            if user:
+                ''' The login is the function call and the user object
+                is the variable defined under the if statement.'''
+                login(request, user)
+                return HttpResponseRedirect(reverse("homepage"))
+    form = LoginForm()
+    context = {"form": form}
+    return render(request, html, context)
 
 def logout_view(request):
     pass
