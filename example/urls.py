11c294649a18402d2d6a8e7dc913b41c763ce483
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-02 21:35:04 -0400

    modified path or urls.py.

--- example/urls.py
+++ example/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from example import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="homepage"),
     path('news_add/', views.news_add), 
 ]
