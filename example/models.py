d521bdd504facd8371244b5e2e6ed7f279463bf9
prodhanm <mustafizur.prodhan@gmail.com>
2022-06-06 19:48:16 -0400

    modified the models to incorporate user and deleted the db plus migrations to create new ones.

--- example/models.py
+++ example/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from django.utils import timezone
+'''For the purpose of this exercise, the User model here, 
+is being tied to the authentication code and the model itself,
+to where the User's data is tied to everything securely. This is
+a fairly simple way to tie data of a User to their authentication.'''
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
+    '''The OneToOne field is used to ensure that a user is tied to
+    one author and that everything of that one author is tied to the 
+    author/user. There can not be any other user to a user profile. 
+    This also means deleting our migrations and database to create an integrity
+    of the data, or risk the data being error ridden. We need to recreate
+    our superuser. This model also ensures that the author is tied to the user.
+    Ex. The author Mustafa Prodhan is the user ie superuser, mustafa.'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
